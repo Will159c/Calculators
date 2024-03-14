294f39a5ede9eae66a1e50e6a4e5dac0c0dcec63
Dane Verkouteren <dane.verkouteren@gmail.com>
2024-03-13 20:26:00 -0700

    added check for zero method and fixed createValue

--- MatrixMethods.java
+++ MatrixMethods.java
@@ -1,20 +1,77 @@
+import java.util.*;
 public class MatrixMethods{
+    int row;
+    int column;
+    int[][] theMatrix;
 
+    public MatrixMethods(){
 
-    public static void CheckForZeros(int Matrix[][]){
-        for(int [] row: Matrix){
-            
-            for(int cell: row){
-                if(cell == 3){
-                    //write code to swap the zeros in the rows to fit with RRE
-                    System.out.println(cell);
-                }
-                
+        Scanner input = new Scanner(System.in);
+        //inputs
+        System.out.println("How many rows:");
+        this.row = input.nextInt();
+        System.out.println("How many columns:");
+        this.column = input.nextInt();
+
+        this.theMatrix = new int[row][column];
+
+    }
+
+    public void WriteMatrixValues() {
+        Scanner in = new Scanner(System.in);
+        int dataEntered = 1;
+        for (int i = 0; i <= row - 1; i++) {
+            for (int j = 0; j <= column - 1; j++) {
+                System.out.println("Enter value for row, column: " + (i+1) + ", " + (j+1));
+                dataEntered = in.nextInt();
+                theMatrix[i][j] = dataEntered;
             }
         }
 
-        for (int i = 0; i < Matrix.length; i++) {
-            System.out.println(Matrix[(i+3)%3][i]);
+    }
+
+    public void CheckForZeros(){
+        int[] tempRow = new int[row];
+        for (int i = 0; i < theMatrix.length; i++) {
+            for (int j = 0; j < theMatrix[0].length; j++) {
+                if(theMatrix[i][j] == 0){
+                    tempRow = theMatrix[i];
+                    theMatrix[i] = theMatrix[Math.min(j+1, row-1)];
+                    theMatrix[Math.min(j+1, row-1)] = tempRow;
+                }
+            }
+        }
+        //currently not working
+        //make sure there are no zeros on the diagonal
+        // for (int i = 0; i < row - 1; i++) {
+        //     for (int j = 0; j < column - 1; j++) {
+        //         if (i == j && theMatrix[i][j]==0) {
+        //             for (int[] element : theMatrix) {
+        //                 if(element[i] != 0){
+        //                     tempRow = theMatrix[i];
+        //                     theMatrix[i] = element;
+        //                     element = tempRow;
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+
+        }
+    
+    public void RREF(){
+        //check for zeros
+
+        //RREF
+    }
+
+    public void printMatrix(){
+        System.out.println("Your Matrix: ");
+        for (int i = 0; i < theMatrix.length; i++) {
+            for (int j = 0; j < theMatrix[0].length; j++) {
+                 System.out.print(theMatrix[i][j] + "\t");
+            }
+            System.out.println();
         }
     }
 }
--- RRE_Form.java
+++ RRE_Form.java
@@ -1,21 +1,14 @@
-import java.util.*;
+// import java.util.*;
 
-public class RRE_Form{
+public class RRE_Form {
     public static void main(String[] args) {
-        System.out.println("Enter your 3x3 matrix, row by row");
-        Scanner input = new Scanner(System.in);
+
+        MatrixMethods i = new MatrixMethods();
+        i.WriteMatrixValues();
+        i.printMatrix();
+        i.CheckForZeros();
+        i.printMatrix();
         
-        int[][] Matrix = {
-            {input.nextInt(),input.nextInt(),input.nextInt()},
-            {input.nextInt(),input.nextInt(),input.nextInt()},
-            {input.nextInt(),input.nextInt(),input.nextInt()}
-        };
-
-        MatrixMethods.CheckForZeros(Matrix);
-
-        input.close();
-
     }
 
 }
-
